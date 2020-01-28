2343f3d61992fc18b0ed008b83d9d04adf42005d
pendato23 <ldu50@naver.com>
2020-01-28 11:04:05 +0900

    create to ex1.js

---
+++ ex1.js
@@ -0,0 +1,23 @@
+var text1 = "text1";
+console.log(text1);
+
+if (true) {
+  var text2 = "text2";
+  console.log(text2);
+}
+
+let text3 = "text3";
+const text4 = "text4";
+console.log(text3);
+console.log(text4);
+text3 = "hellow";
+console.log(text3);
+text3 = "500";
+//text4 = "black";
+console.log(text4);
+
+if (true) {
+  let text5 = "hi"; //같은 블록 안에서만 통용
+  console.log(text5);
+}
+//console.log(text5);
